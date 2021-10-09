05655048f27103e71ad5f2f5057d5b003213e843
Aromal Mihraj <aromalmihraj42@gmail.com>
2021-10-09 19:32:45 -0400

    Update corona.py

--- corona.py
+++ corona.py
@@ -1,4 +1,5 @@
 import requests
+import sys
 
 api_key = "https://api.covid19api.com/summary"
 json_data = requests.get(api_key).json()
@@ -6,16 +7,26 @@ total_cases = str(json_data['Global']['TotalConfirmed'])
 total_deaths = str(json_data['Global']['TotalDeaths'])
 countries = json_data['Countries']
 
-print("Total Cases: " + total_cases + "\nTotal Deaths: " + total_deaths)
+print("Total Cases: " + total_cases + "\nTotal Deaths: " + total_deaths + "\n")
 
 def shorten(s, subs):
     i = s.index(subs)
     return s[:i]
 
-country = input("\nType a country's name to see its stats: ")
+def country_stat():
+  print("Press Ctrl + C to exit")
+  country = input("Type a country's name to see its stats: ")
 
-for i in countries:
-  if i["Slug"] == country.lower():
-    print("The total confirmed cases in %s is %s" % (country, i['TotalConfirmed']))
-    print("The total deaths in %s is %s" % (country, i['TotalDeaths']))
-    print("Date updated: %s" % shorten(i['Date'], 'T'))
+  for i in countries:
+    if i["Slug"] == country.lower():
+      print("\nThe total confirmed cases in %s is %s" % (country, i['TotalConfirmed']))
+      print("The total deaths in %s is %s" % (country, i['TotalDeaths']))
+      print("Date updated: %s \n" % shorten(i['Date'], 'T'))
+      country_stat()
+
+try:
+  country_stat()
+
+except KeyboardInterrupt:
+  print("\n")
+  sys.exit()
